4cd8f8036de23228c270747a983ae5e0e2b14354
Moyo2100 <moises.rojas@leon.uia.mx>
2019-03-14 08:27:31 -0600

    Proyecto con opción de resize

--- admin/includes/insertar-producto.php
+++ admin/includes/insertar-producto.php
@@ -29,17 +29,47 @@ $nombre_final_archivo = date("His") . $nombre_archivo;
 //MOVEMOS EL ARCHIVO DE LA CARPETA TEMPORAL A LA CARPETA IMG
 move_uploaded_file($tmp_archivo,$directorio_archivos . $nombre_final_archivo);
 
+//RUTA DEL THUMB
+$directorio_thumb = '../img/portadas/thumbs/thumb_';
+$nombre_final_thumb = $directorio_thumb . $nombre_final_archivo;
+
+//DEFINIMOS EL ARCHIVO DE ORIGEN (ABRIR)
+$imagen_o = imagecreatefromjpeg($directorio_archivos . $nombre_final_archivo);
+
+//DEFINIMOS EL NUEVO TAMAÑO
+$width = 300;
+$height = 300;
+
+//LEEMOS las dimensiones del archivo de origen
+$width_o = imagesx($imagen_o);
+$height_o = imagesy($imagen_o);
+
+//HACEMOS EL RESIZE DE MANERA PROPORCIONAL
+$proporcion = $width_o/$height_o;
+if ($width/$height > $proporcion){
+	$width = $height*$proporcion;
+} else {
+	$height = $width/$proporcion;
+}
+
+//DEFINIMOS EL CANVAS DE LA NUEVA IMAGEN
+$imagen_t = imagecreatetruecolor($width,$height);
+
+//ESCRIBIMOS EL CANVAS CON LA SIGUIENTE INFORMACIÓN
+imagecopyresampled($imagen_t, $imagen_o, 0,0,0,0, $width, $height, $width_o, $height_o);
+imagejpeg($imagen_t, $nombre_final_thumb, 90);
+
 } else { 
 	echo "Sólo puedes subir imágenes .jpg, .gif o .png";
 }
 
 //DECLARAMOS LA SENTENCIA SQL PARA INGRESAR LOS DATOS A LA TABLA PRODUCTOS
-$consulta_insertar = "INSERT INTO Productos (clave_producto, nombre_producto, descripcion_producto, precio, fecha_lanzamiento) VALUES ('$clave','$nombre','$descripcion',$precio,'$fecha')";
+$consulta_insertar = "INSERT INTO Productos (clave_producto, nombre_producto, descripcion_producto, imagen_producto, precio, fecha_lanzamiento, thumb_imagen) VALUES ('$clave','$nombre','$descripcion','$nombre_final_archivo',$precio,'$fecha', '$nombre_final_thumb')";
 
 //EJECUTAMOS LA CONSULTA CON LA CONEXIÓN Y LA SENTENCIA SQL INSERT INTO
-//mysqli_query($conexion, $consulta_insertar);
+mysqli_query($conexion, $consulta_insertar);
 
 //REDIRECCIONAMOS AL ADMINISTRADOR AL INDEX DE ADMIN
-//header('Location:../index.php');
+header('Location:../index.php');
 ob_end_flush();
 ?>
